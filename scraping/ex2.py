5203475354be00451d779bbfc2e98459bbd9647f
Laperavee <tomlebonpro@gmail.com>
2023-11-14 17:37:00 +0100

    Printing country information for each country

--- scraping/ex2.py
+++ scraping/ex2.py
@@ -5,3 +5,8 @@ URL = "https://www.scrapethissite.com/pages/simple/"
 response = requests.get(URL)
 html = response.text
 soup = bs(html,'html.parser')
+countries = soup.find_all('div',{'class':'col-md-4 country'})
+for country in countries:
+    country_name = country.find("h3").get_text().replace('\n',"").replace("  ","")
+    country_capital = country.find("span",{"class":"country-capital"}).get_text().replace('\n', "").replace("  ", "")
+    print(country_name, "capitale =",country_capital)
