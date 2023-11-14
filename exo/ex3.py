ebe33fc338f853f995166216d4a543a430ae96d9
Laperavee <tomlebonpro@gmail.com>
2023-11-14 15:43:00 +0100

    non-functional version

--- exo/ex3.py
+++ exo/ex3.py
@@ -9,5 +9,19 @@ def divide(a,b):
 
 chain = input("Saisissez une opération")
 list = chain.replace("+"," + ").replace("-"," - ").replace("/"," / ").replace("*"," * ")
-
-
+sum = 0
+for i in range(0,len(list)):
+    result = 0
+    if list[i] == "*":
+        result = multiply(int(list[i-1]),int(list[i+1]))
+    if list[i] == "/":
+        result = divide(int(list[i - 1]),int(list[i + 1]))
+    if list[i] == "*":
+        result = add(int(list[i - 1]), int(list[i + 1]))
+    if list[i] == "*":
+        result = substract(int(list[i-1]),int(list[i+1]))
+    list[i] =""
+    list[i+1] = ""
+    list[i-1].replace(list[i-1],result)
+    sum+=result
+    print(list)
