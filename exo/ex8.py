5f24b2a7f7b334891beda5c669012fe1d6c7dcdc
Laperavee <tomlebonpro@gmail.com>
2023-11-14 16:52:24 +0100

    best student search without equality possibility

--- exo/ex8.py
+++ exo/ex8.py
@@ -1,12 +1,19 @@
 import random
 class1 = []
 eleves = {"John Doe","Mac Miller","Jack Sparrow","Pablo Escobar","Chris Brown","Thomas Muller","Max Verstappen","Alex Hitchens"}
-for eleve in eleves:
+for names in eleves:
     eleveObj = {}
     notes = []
     for i in range (0,5):
         notes.append(random.randint(0,20))
-    eleveObj["nom"] = eleve.split(" ")[1]
-    eleveObj["prenom"] = eleve.split(" ")[0]
+    eleveObj["name"] = names.split(" ")[1]
+    eleveObj["surname"] = names.split(" ")[0]
     eleveObj["notes"] = notes
     class1.append(eleveObj)
+
+best_student = {}
+max_note = 0
+for eleve in class1:
+    if max(eleve["notes"]) > max_note:
+        best_student = eleve["name"], eleve["surname"]
+print(best_student)
