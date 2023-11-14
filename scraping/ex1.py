c4a38a4eb176f5ff1ca4ffefa588c7977fff9a90
Laperavee <tomlebonpro@gmail.com>
2023-11-14 17:29:33 +0100

    Print the countries and removing \n and spaces

--- scraping/ex1.py
+++ scraping/ex1.py
@@ -5,3 +5,6 @@ URL = "https://www.scrapethissite.com/pages/simple/"
 response = requests.get(URL)
 html = response.text
 soup = bs(html,'html.parser')
+countries = soup.find_all('h3',{'class':'country-name'})
+for country in countries:
+    print(country.getText().replace('\n',"").replace("  ",""))
