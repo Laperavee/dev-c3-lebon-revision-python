a3edcd13417ab0d242b2788efbe5cb4c38f7c96e
Laperavee <tomlebonpro@gmail.com>
2023-11-14 17:53:42 +0100

    Complete version with printing and parsing methods

--- scraping/ex3.py
+++ scraping/ex3.py
@@ -7,4 +7,8 @@ html = response.text
 soup = bs(html,'html.parser')
 teams = soup.find_all('tr',{'class':'team'})
 for team in teams:
-    print(team.get_text().replace('\n',"").replace("   ",""))
+    team_info = team.find_all('td')
+    team_inf = []
+    for info in team_info:
+        team_inf.append(info.get_text().replace("  ","").replace('\n',''))
+    print(team_inf[0],team_inf[1],team_inf[2],team_inf[5],"%")
