287e3bed10826a69ac3404457cf55f2d8432ef81
Laperavee <tomlebonpro@gmail.com>
2023-11-14 16:01:22 +0100

    Functions calls and result prints

--- exo/ex4.py
+++ exo/ex4.py
@@ -14,5 +14,13 @@ a = 0
 b = 10
 localmin = b
 localmax = a
+sum = 0
 for i in range(0,10):
     list.append(random.randint(a,b))
+for item in list:
+    localmin = inf(localmin,item)
+    localmax = sup(localmax,item)
+    sum+=item
+print("Local min =" ,localmin)
+print("Local max =" ,localmax)
+print("Average =", sum / len(list))
