5bd0a92634fe9249ed67c1de5c8dc5ff3946ab7f
Laperavee <tomlebonpro@gmail.com>
2023-11-14 17:18:48 +0100

    Adding main

--- exo/ex10.py
+++ exo/ex10.py
@@ -1,10 +1,12 @@
 def InsertionSorting(tab):
-    x = len(tab)
-    for i in range(1,x):
-        index = tab(i)
+    for i in range(1,len(tab)):
+        index = tab[i]
         j = i-1
-        while j>=0 and tab(j) > index:
-            tab[j+1] = tab(j)
+        while j>=0 and tab[j] > index:
+            tab[j+1] = tab[j]
             j = j-1
         tab[j+1] = index
 
+a = [1,6,8,4,8,5,4,7,6,9]
+InsertionSorting(a)
+print(a)
