1c6a4bccc36a3919ba842230770519bb9ab23c21
Laperavee <tomlebonpro@gmail.com>
2023-11-15 15:37:31 +0100

    Looped version to get the result of the first 3 pages

--- scraping/ex3.py
+++ scraping/ex3.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
-URL = "https://www.scrapethissite.com/pages/forms/?page_num=1"
-response = requests.get(URL)
-html = response.text
-soup = bs(html,'html.parser')
-teams = soup.find_all('tr',{'class':'team'})
-for team in teams:
-    team_info = team.find_all('td')
-    team_inf = []
-    for info in team_info:
-        team_inf.append(info.get_text().replace("  ","").replace('\n',''))
-    print(team_inf[0],team_inf[1],team_inf[2],team_inf[5],"%")
+# I prefer declare a variable to the number of pages, it's easier to change it
+nbpages = 3
+for i in range(0,nbpages):
+    URL = f"https://www.scrapethissite.com/pages/forms/?page_num={i+1}"
+    response = requests.get(URL)
+    html = response.text
+    soup = bs(html,'html.parser')
+    teams = soup.find_all('tr',{'class':'team'})
+    for team in teams:
+        team_info = team.find_all('td')
+        team_inf = []
+        for info in team_info:
+            team_inf.append(info.get_text().replace("  ","").replace('\n',''))
+        print(team_inf[0],team_inf[1],team_inf[2],team_inf[5],"%")
